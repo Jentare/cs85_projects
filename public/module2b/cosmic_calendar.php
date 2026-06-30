948fc4e10fbcbe05f7276cac38168711c21af214
Jeni <frei_jennifer_charle@student.smc.edu>
2026-06-30 00:13:43 -0700

    mod2

--- public/module2b/cosmic_calendar.php
+++ public/module2b/cosmic_calendar.php
@@ -23,6 +23,11 @@
                 $jsonString = @file_get_contents('https://timeapi.io/api/time/current/zone?timeZone=America%2FLos_Angeles');
                 $data = json_decode($jsonString);
              
+                $nameLength = strlen($firstName);
+                $dateTimeString = $data->dateTime;
+                $date = new DateTime($dateTimeString);
+                $dayOfYear = (int)$date->format('z') + 1;
+                $month = $data->month;
                 
         </div>
     </div>
