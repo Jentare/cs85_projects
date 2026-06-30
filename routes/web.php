70e2e45a0d7eeb0184f7864913df8d6e657f2c9f
Jeni <frei_jennifer_charle@student.smc.edu>
2026-06-29 23:51:41 -0700

    mod2

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Hello World from Jeni!';
 });
-Route::get('/module2a/price-engine', function () {
-    require base_path('module2a/price_engine.php');
-});
-Route::get('/module2a/price-engine-ref', function () {
-    require base_path('module2a/price_engine_refactored.php');
-});
