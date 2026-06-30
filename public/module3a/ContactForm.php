697cc146bb4143051b039cfede61fa43f6647edb
Jeni <frei_jennifer_charle@student.smc.edu>
2026-06-30 12:36:32 -0700

    mod3a added phptags

--- public/module3a/ContactForm.php
+++ public/module3a/ContactForm.php
@@ -1,7 +1,17 @@
+<?php
+// adds a function called validateInput that takes two parameters $data and $fieldName and uses a global variable $errorCount 
 function validateInput($data, $fieldName){
     global $errorCount;
     if(empty($data)){
         echo "\"$fieldName\" is a required field. <br />\n";
-        
+        ++$errorCount;
+        $retval = "";
     }
+    else {
+        $retval = trim($data);
+        $retval = stripslashes($retval);
+    }
+return($retval);
 }
+
+?>
